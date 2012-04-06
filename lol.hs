f0b5d4322ff6cc0798cde66c6f5181d50fbd12a0
Niklas Hambüchen <mail@nh2.me>
2012-04-06 23:04:48 +0100

    Some haskell serverstarter improvements

--- lol.hs
+++ lol.hs
@@ -4,25 +4,36 @@ import System.Posix.Process
 import System.Posix.Signals
 import System.Posix.Types
 import Control.Concurrent
+import Control.Monad (forM_)
 
 processes :: [(String, [String])]
-processes = [ ("runhaskell", ["gee.hs"])
+processes = [ ("./dist/build/testserver/testserve", [])
+            , ("bash", ["-c", "python3 -m http.server 7357 || python -m SimpleHTTPServer 7357"])
             ]
 
 main :: IO ()
 main = do
     pList <- newMVar []
-    installHandler sigINT
-       (Catch $ term pList) Nothing
+    sema <- newMVar ()
+    _ <- takeMVar sema
+    installHandler sigINT (Catch $ intHandler pList sema) Nothing
     mapM_ (spawn pList) processes
-    threadDelay 3000000
-    term pList
+    --threadDelay 1500000
+    withMVar pList print
+    takeMVar sema
+    --threadDelay 1500000
+    --term pList
+
+intHandler :: MVar [ProcessID] -> MVar () -> IO ()
+intHandler pList sema = term pList >> putMVar sema ()
 
 term :: MVar [ProcessID] -> IO ()
-term pList = withMVar pList . mapM_ $ \p -> do
-    putStrLn $ "Killing process tree of " ++ show p
-    executeFile "bash" True ["killTree.sh", show p] Nothing
-    -- signalProcess sigTERM p
+term pList = modifyMVar_ pList $ \l -> do
+    forM_ l $ \p -> do
+        putStrLn $ "Killing process tree of " ++ show p
+        forkProcess $ executeFile "bash" True ["killTree.sh", show p] Nothing
+        -- signalProcess sigTERM p
+    return []
 
 spawn :: MVar [ProcessID] -> (FilePath, [String]) -> IO ProcessID
 spawn pList (path, args) = do
